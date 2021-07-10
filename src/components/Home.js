8d15957718dabb444090457d085129aaabf0ce2f
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-10 19:13:06 -0400

    update bg image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import image from '../images/palm.jpeg'
+import image from '../images/palm.jpg'
 
 export default function Home(){
     return (
         <main>
-            <img src={image} alt="Palm Leaves" className="absolute bg-center bg-fixed bg-cover h-full w-full" />
+            <img src={image} alt="Palm Leaves" className="absolute h-full w-full" />
             <section className="relative lg:flex justify-center min-h-screen pt-12 lg:pt-64 lg:px-8 ">
                 <h1 className="text-center text-3xl lg:text-6xl text-green-100 font-bold cursive lg:home-name">Hello. I'm <span className="text-red-400">Michael</span></h1>
             </section>
