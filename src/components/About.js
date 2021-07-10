6a9949c921f06a38cc8076483b246d0d9824c05d
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-09 21:13:37 -0400

    tweaked about page tailwind

--- src/components/About.js
+++ src/components/About.js
@@ -33,21 +33,30 @@ export default function About() {
         alt="Florida BG background"
         className="absolute object-cover w-full h-full"
       />
-      <div className="p-10 lg:pt-48 container mx-auto relative">
-        <section className="bg-blue-400 bg-opacity-75 rounded-lg shadow-2xl lg:flex p-20">
+      <div className="py-10">
+        </div>
+      <div className="lg:pt-48 container mx-auto relative">
+        <section className="bg-red-200 bg-opacity-50 rounded-lg shadow-2xl lg:flex p-20">
           <img
             src={urlFor(author.authorImage).url()}
             className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
             alt="Michael"
           />
           <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive text-6xl text-green-300 mb-4">
+            <h1 className="cursive text-6xl text-blue-700 mb-4">
               Hey there. I'm{" "}
-              <span className="text-green-100">{author.name}</span>
+              <span className="text-red-800">{author.name}</span>
             </h1>
-            <p className="text-green-200 text-lg">{author.bio}</p>
+            <p className="text-red-700 text-lg">{author.bio}</p>
+            <div className="text-blue-800 cursive">
+          <h2>Get In Touch</h2>
+          <p className="text-lg flex flex-col justify-center">Wanna talk about code? New Opportunities? Anything? Reach out about anything below.</p>
+          <a className="hover:text-red-200" target="_blank" href="mailto:mcooper7649@gmail.com"><i className="text-red-800  fas fa-4x fa-envelope-square"></i></a>
+        </div>
           </div>
         </section>
+        <div className="py-40">
+        </div>
       </div>
     </main>
   );
