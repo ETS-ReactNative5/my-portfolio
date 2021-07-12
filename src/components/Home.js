499a69f1d6a5845fbec52d4bd16b294054dcd1aa
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-12 12:08:04 -0400

    fixed home mobile layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,10 +32,10 @@ class Home extends Component{
     //    }
     render(){ 
     return (
-        <main style={{backgroundImage: "url(" + Background + ")"}} className="bg-cover">
+        <main style={{backgroundImage: "url(" + Background + ")"}} className="min-w-screen bg-cover">
             {/* <img src={image} alt="Palm Leaves" className="absolute h-full lg:w-full" /> */}
-            <section className="relative justify-center min-h-screen pt-12 lg:pt-64 lg:px-8 ">
-            <div className="hover:bg-gray-500 bg-yellow-200 shadow-2xl bg-opacity-75 lg:rounded-full">
+            <section className="relative justify-center min-h-screen pt-64 lg:px-8 ">
+            <div className="hover:bg-gray-500 mx-4 rounded bg-yellow-200 shadow-2xl bg-opacity-75 lg:rounded-full">
                 <h1 className="pt-3 text-center text-4xl lg:text-6xl text-green-100 font-bold cursive lg:home-name">Hello. I'm <span className="text-red-500">Michael</span></h1>
                 <h2 className="pt-20 pb-10 mx-20 text-center text-2xl lg:text-3xl text-purple-600 font-bold cursive lg:home-name">"{this.state.quote}"</h2>
                 <h2 className="pb-3 mx-20 text-center text-2xl lg:text-3xl text-purple-600 font-bold cursive lg:home-name">-{this.state.author}</h2>
