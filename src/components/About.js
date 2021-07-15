51b3112f04df4154c0b8b560ec96734b75377daa
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-15 08:27:47 -0400

    updated fonts on blog and singlepost, then changed about fonts and palette

--- src/components/About.js
+++ src/components/About.js
@@ -41,20 +41,20 @@ export default function About() {
   return (
     <main style={{backgroundImage: "url(" + Background + ")"}} className="bg-cover relative">
       <div className="lg:pt-48 p-3 lg:px-20 px-3 mx-auto relative">
-        <section className="bg-red-200 bg-opacity-50 rounded-lg shadow-2xl lg:flex lg:p-20 py-6 px-3">
+        <section className="bg-black bg-opacity-50 rounded-lg shadow-2xl lg:flex lg:p-20 py-6 px-3">
           <img
             src={urlFor(author.authorImage).url()}
             className="shadow-2xl rounded-full w-44 h-44 lg:w-64 lg:h-64 mr-8"
             alt="Michael"
           />
           <div className="rounded text-lg bg-red-200 bg-opacity-75 mt-3">
-            <h1 className="p-2 text-center cursive text-3xl lg:text-5xl text-blue-700 mb-4">
+            <h1 className="p-2 text-center cursive text-3xl lg:text-5xl text-gray-100 mb-4">
               Hey there. I'm{" "}
               <span className="text-red-700">{author.name}</span>
             </h1>
-            <p className="text-center text-red-700 lg:text-2xl cutive">{author.bio}</p>
-            <div className="text-blue-700 cursive">
-          <h2 className="text-center text-red-700 text-3xl py-3">Get In Touch</h2>
+            <p className="text-center text-red-700 lg:text-2xl">{author.bio}</p>
+            <div className="text-white">
+          {/* <h2 className="text-center text-red-700 text-2xl py-3">Get In Touch</h2> */}
           <div className="lg:flex p-3 text-center">
           <p className="text-xl pb-5 pr-5">Wanna talk about code? New Opportunities? Anything? Reach out about anything below.</p>
           <Button className="" target="_blank" rel="noreferrer" href="mailto:mcooper7649@gmail.com" variant="contained" color="secondary">
