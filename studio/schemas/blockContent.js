7f6a1d2a3d58fc64ead228def35f8645ba36efb0
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-17 11:50:48 -0400

    update sanity schema

--- studio/schemas/blockContent.js
+++ studio/schemas/blockContent.js
@@ -26,9 +26,14 @@ export default {
         {title: 'H2', value: 'h2'},
         {title: 'H3', value: 'h3'},
         {title: 'H4', value: 'h4'},
+        {title: 'H5', value: 'h5'},
+        {title: 'H6', value: 'h6'},
         {title: 'Quote', value: 'blockquote'},
       ],
-      lists: [{title: 'Bullet', value: 'bullet'}],
+      lists: [
+        {title: 'Bullet', value: 'bullet'},
+        {title: 'Number', value: 'number'}
+      ],
       // Marks let you mark up inline text in the block editor.
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
@@ -64,5 +69,8 @@ export default {
       type: 'image',
       options: {hotspot: true},
     },
+    {
+      type: 'code'
+    }
   ],
 }
