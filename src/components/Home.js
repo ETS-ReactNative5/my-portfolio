bcfac4bcd9df26224f6755714c2505b38f2b1640
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-01-22 10:33:04 -0500

    fixed home quote styles and added more context

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,32 +16,32 @@ class Home extends Component {
       console.log(resp.data);
       setTimeout(() => {
         this.setState({
-          quote: 'I built this place to help inspire young ninjas',
+          quote: 'This Place Is To Help Inspire Fellow Ninjas',
           author: Date().slice(0, 16),
         });
       }, 2000);
       setTimeout(() => {
         this.setState({
-          quote: 'Please check out my blog',
+          quote: 'Please Check Out My Blog',
           author: Date().slice(0, 16),
         });
       }, 4000);
       setTimeout(() => {
         this.setState({
-          quote: 'Subscribe and view my Projects too!',
+          quote: 'Subscribe And View My Projects Too!',
           author: Date().slice(0, 16),
         });
       }, 6000);
       setTimeout(() => {
         this.setState({
-          quote: 'Check out this motivational quote and try to stay motivated!',
+          quote: 'Read This Famous Quote And Stay Motivated!',
           author: Date().slice(0, 16),
         });
       }, 8000);
       setTimeout(() => {
         this.setState({
-          quote: resp.data.content,
-          author: resp.data.author,
+          quote: `${`"` + resp.data.content + `"`}`,
+          author: `${`-` + resp.data.author}`,
         });
       }, 10000);
     });
@@ -51,12 +51,12 @@ class Home extends Component {
     return (
       <main className="bg-img pt-32 min-w-screen bg-cover">
         <section className="relative justify-center h-full lg:px-8 lg:mx-20">
-          <div className="h-auto grid p overflow-auto min-w-screen wrapper hover:bg-gray-500 mx-4 rounded bg-red-400 shadow-2xl bg-opacity-100 lg:rounded-full">
+          <div className="h-auto grid p overflow-auto min-w-screen wrapper  mx-4 rounded bg-red-400 shadow-2xl bg-opacity-100 lg:rounded-full ">
             {/* <h1 className="pt-3 text-center text-4xl lg:text-6xl text-green-100 font-bold cursive lg:home-name">Hello. I'm <span className="text-purple-800">Michael</span></h1> */}
-            <h2 className="lg:px-20 break-words pt-4 lg:pt-20 lg:pb-10 text-center text-3xl md:text-5xl text-purple-800 font-bold cursive lg:home-name">
+            <h2 className="myText lg:px-20 break-words pt-4 lg:pt-20 lg:pb-10 text-center text-3xl md:text-5xl text-purple-800 font-bold cursive lg:home-name">
               {this.state.quote}
             </h2>
-            <h2 className="break-words text-center m-auto text-3xl text-blue-600 font-bold cursive lg:home-name">
+            <h2 className="myText break-words text-center m-auto text-3xl text-blue-600 font-bold cursive lg:home-name">
               {this.state.author}
             </h2>
           </div>
